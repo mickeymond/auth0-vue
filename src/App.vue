16648d918fce8e82bf1e2a105e8e4cb1e4ab806e
Michael Hammond <mickeymond@gmail.com>
2020-07-22 07:14:39 +0100

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Is Logged In: {{ $auth.isAuthenticated }}</h1>
+
+    <div v-if="$auth.isAuthenticated">
+      <img height="100" :src="$auth.user.picture">
+      <h2>Name: {{ $auth.user.name }}</h2>
+      <p>Email: {{ $auth.user.email }}</p>
+      <div>
+        <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+      </div>
+    </div>
+
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout();
+    }
+  },
+  watch: {
+    '$auth.isAuthenticated': (value) => {
+      console.log(value);
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId
+});
+
 Vue.config.productionTip = false
 
 new Vue({
